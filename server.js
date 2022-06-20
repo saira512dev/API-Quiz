2626fc976adefe1a96b5c24ecb5afb360397b932
saira512dev <512saira@gmail.com>
2022-06-20 22:32:19 +0530

    Started with Quiz API project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 8000
+
+const questions = [
+    {
+        'question': '_____ in JS, have no properties',
+        'choices': ["arrays","strings","null","sets"],
+        'answer':"null",
+        'info' : "Null and Undefined in Javascript has no properties"
+    }
+]
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api',(req, res) => {
+    res.json(questions)
+})
+
+app.listen(PORT,() => {
+    console.log("SERVER IS UP & RUNNING");
+})
