1de84e341b076f151c665fc852d1d76e1d1dfe19
saira512dev <512saira@gmail.com>
2022-06-25 22:03:56 +0530

    fixing post

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ MongoClient.connect(
         console.log(req)
       scores.insertOne(req.body)
         .then((result) => {
-          res.send(result)
+          
         })
         .catch((error) => console.error(error));
     });
