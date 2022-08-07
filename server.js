8c2f5e34836ae02878fc014a765e14045758ced3
saira512dev <512saira@gmail.com>
2022-08-07 14:02:28 +0530

    Question validation

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const ObjectId = require("mongodb").ObjectID;
 const multer = require('multer');
 const upload = multer();
 const app = express();
-const { check, validationResult } = require('express-validator');
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(upload.array()); 
 app.use(express.static('public'));
+const { check, validationResult } = require('express-validator');
+
 
 const PORT = 8000
 
@@ -108,7 +109,7 @@ MongoClient.connect(
 
     app.post("/api/questions/add",[
         check('question').exists().withMessage('Question cannot be empty.'),
-        check('choices').exists().withMessage('Choices cannot be empty.'),
+        check('choices').isEmpty().withMessage('Choices cannot be empty.'),
         check('answer_index').exists().withMessage('Answer index cannot be empty.'),
         check('info').exists().withMessage('Info cannot be empty.'),
     ] ,(req, res) => {
