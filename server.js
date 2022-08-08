ec4225938b0f640e3f57449be0f4be411b7c68da
saira512dev <512saira@gmail.com>
2022-08-08 13:16:02 +0530

    Fixing get question by id

--- server.js
+++ server.js
@@ -127,9 +127,9 @@ MongoClient.connect(
     });
     app.get("/api/questions/view/:id",(req, res) => {
         console.log(ObjectId(req.params.id))
-        questions.findOne({"_id" : new ObjectId(req.params.id)})
+        questions.find(ObjectId(req.params.id)).toArray()
         .then((result) => {
-           return res.status(200).send(result)
+           return res.status(200).json(result)
         })
         .catch((error) => console.error(error));
     });
